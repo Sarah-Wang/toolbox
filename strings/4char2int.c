0228c11f1954736be69ed81f7b764239f9bbb624
Sarah-Wang <yyzxml@outlook.com>
2019-12-23 15:13:54 +0800

    feat: add char to int example
    Signed-off-by: Sarah-Wang <yyzxml@outlook.com>

---
+++ strings/4char2int.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void convert_chars_2_int(unsigned char a[], unsigned int *res)
+{
+	/* unsigned char a[4] = {0x00, 0x01, 0x02, 0x03}; */
+	unsigned int b;
+
+	b = (a[0] & 0x000000ff) |\
+	    ((a[1] << 8) & 0x0000ff00) |\
+	    ((a[2] << 16) & 0x00ff0000) |\
+	    ((a[3] << 24) & 0xff000000);
+	/* printf("b = 0x%08x\n", b); */
+	*res = b;
+}
+
+void main(void)
+{
+	unsigned char a[4] = {0x00, 0x01, 0x02, 0x03};
+	unsigned int b;
+
+	convert_chars_2_int(a, &b);
+	printf("b = 0x%08x\n", b);
+}
